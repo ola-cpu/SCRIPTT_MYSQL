f99124d2afc1253b5c47538065ba40585e09974d
ola-cpu <68545167+ola-cpu@users.noreply.github.com>
2022-03-08 21:14:45 +0100

    debut de la solution 1

---
+++ empireoba.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+echo "************Correction socket '/var/run/mysqld.sock' (2) par EMPIRE_OBA**********"
+
+sudo su 
+
+ls /var/run/my*
+
+mkdir /var/run/mysqld
+
+ls /var/run/my*
+
+ls -lart /var/run/my*
+
+touch /var/run/mysqld/mysqld.sock
+
+ls -lart /var/run/my*
+
+chown -R mysql /var/run/mysqld
+
+ls -lart /var/run/my*
+
+/etc/init.d/mysql restart
+
+mysql -u root 
+
+
+echo "************FIN DU SCRIPT**********"
+
+
